39de8aac932c93f12b23160195fe1e80192b5f54
Davih Duque <davihduque8@gmail.com>
2023-10-02 10:12:05 -0300

    Mudanças pedro

--- src/Eventos.java
+++ src/Eventos.java
@@ -14,6 +14,9 @@ public class Eventos {
     // Contador para o número de ingressos vendidos para este evento
     int numIngressos;
 
+    // Limitador de ingressos que será utilizado nas subclasses de Eventos
+    int LIMITE_INGRESSOS;
+
     // Constructor de Eventos
     public Eventos(String nome, Boolean eAcessivel, Ingresso ingresso) {
         this.nome = nome;
@@ -25,6 +28,8 @@ public class Eventos {
         this.vendasIngressos = new Ingresso[1000];
         // Inicializa o número de ingressos vendidos como 0, necessario para "extrato" depois
         this.numIngressos = 0;
+        // Inicializa 0 número limite de ingressos vendidos como 0, para obter seu limite real em cada subclasse de evento
+        this.LIMITE_INGRESSOS = 0;
     }
 
     // Método para obter o tipo de evento (será implementado nas subclasses)
@@ -46,4 +51,109 @@ public class Eventos {
     public String toString() {
         return "Nome: " + this.nome + " \nData: " + this.data + "\nÉ acessível? " + eAcessivel() + "\nPreço: " + ingresso.getPreco() + "\n Ingresso: " + ingresso.toString();
     }
+
+    // Método para obter a quantidade de ingressos vendidos
+    public int getNumIngressos() {
+        return numIngressos;
+    }
+
+    // Método para delimitar o limite dos ingressos dos Eventos
+    public void VenderIngressos(){
+        if(numIngressos >= LIMITE_INGRESSOS){
+            System.out.println("Limite de ingressos atingido!");
+        } else{
+            // Adiciona a quantidade de ingressos vendidos até atingir o limite
+            numIngressos++;
+        }
+    }
+
+    public static class Filme extends Eventos{
+
+        public Filme(String nome, Boolean eAcessivel, Ingresso ingresso){
+            //Atributos da superclasse
+            super(nome,eAcessivel, ingresso);
+
+            // Define o limite de ingressos específico para Filme
+            this.LIMITE_INGRESSOS = 200;
+        }
+
+        //Anula o método para delimitar o limite dos ingressos do Filme
+        @Override
+        // Método para delimitar o limite dos ingressos do Filme
+        public void VenderIngressos(){
+
+            if(numIngressos >= LIMITE_INGRESSOS){
+                System.out.println("Limite de ingressos do Filme atingido!");
+            } else{
+                // Adiciona a quantidade de ingressos vendidos até atingir o limite
+                numIngressos++;
+            }
+        }
+
+        @Override
+        // Obtem a quantidade de ingressos vendidos
+        public int getNumIngressos() {
+            return numIngressos;
+        }
+    }
+
+    public static class Concerto extends Eventos{
+
+        public Concerto(String nome, Boolean eAcessivel, Ingresso ingresso){
+            //Atributos da superclasse
+            super(nome,eAcessivel, ingresso);
+
+            // Define o limite de ingressos específico para Concerto
+            this.LIMITE_INGRESSOS = 3;
+        }
+
+        //Anula o método para delimitar o limite dos ingressos de Concerto
+        @Override
+        // Método para delimitar o limite dos ingressos de Concerto
+        public void VenderIngressos(){
+
+            if(numIngressos >= LIMITE_INGRESSOS){
+                System.out.println("Limite de ingressos do Conserto atingido!");
+            } else{
+                // Adiciona a quantidade de ingressos vendidos até atingir o limite
+                numIngressos++;
+            }
+        }
+        @Override
+        // Obtem a quantidade de ingressos vendidos
+        public int getNumIngressos() {
+            return numIngressos;
+        }
+    }
+
+    public static class Teatro extends Eventos{
+
+        public Teatro(String nome, Boolean eAcessivel, Ingresso ingresso){
+            //Atributos da superclasse
+            super(nome,eAcessivel, ingresso);
+
+            // Define o limite de ingressos específico para Teatro
+            this.LIMITE_INGRESSOS = 250;
+        }
+
+        //Anula o método para delimitar o limite dos ingressos do Teatro
+        @Override
+        // Método para delimitar o limite dos ingressos do Teatro
+        public void VenderIngressos(){
+
+            if(numIngressos >= LIMITE_INGRESSOS){
+                System.out.println("Limite de ingressos do Teatro atingido!");
+            } else{
+                // Adiciona a quantidade de ingressos vendidos até atingir o limite
+                numIngressos++;
+            }
+        }
+
+        @Override
+        // Obtem a quantidade de ingressos vendidos
+        public int getNumIngressos() {
+            return numIngressos;
+        }
+    }
+
 }
--- src/mainTestes.java
+++ src/mainTestes.java
@@ -2,16 +2,31 @@
 public class mainTestes {
     public static void main(String[] args) {
 
-        // Cria um evento chamado JCole com preço 700, acessível, usando ingresso de meia entrada
-        Eventos JCole = new Eventos("J.Cole", true, new Ingresso.meiaEntrada(700));
+        // Cria 3 eventos diferentes para testar a venda de ingressos
+        Eventos tylerConcerto = new Eventos.Concerto("Tyler", true, new Ingresso.meiaEntrada(700));
+        Eventos filmeEvento = new Eventos.Filme("Filme1", false, new Ingresso.meiaEntrada(500));
+        Eventos teatroEvento = new Eventos.Teatro("Teatro1", true, new Ingresso.VIP(800));
 
-        // Cria um evento chamado Joji com preço 700, não acessível, usando ingresso VIP
-        Eventos Joji = new Eventos("Drake", false, new Ingresso.VIP(700));
+        // Venda ingressos para os eventos
+        tylerConcerto.VenderIngressos();
+        tylerConcerto.VenderIngressos();
+        tylerConcerto.VenderIngressos();
+        tylerConcerto.VenderIngressos();
 
-        // Imprime detalhes do evento JCole
-        System.out.println(JCole.toString());
-        System.out.println(); // Adiciona uma linha em branco
-        // Imprime detalhes do evento Joji
-        System.out.println(Joji.toString());
+        //Vendi 4 e coloquei 3 no limite no Concerto para testar se o LIMITE estava funcionando
+
+        filmeEvento.VenderIngressos();
+        filmeEvento.VenderIngressos();
+        filmeEvento.VenderIngressos();
+        filmeEvento.VenderIngressos();
+        filmeEvento.VenderIngressos();
+        filmeEvento.VenderIngressos();
+
+        teatroEvento.VenderIngressos();
+
+        // Imprimindo a quantidade de ingressos vendidos para cada evento
+        System.out.println("Quantidade de ingressos vendidos para Concerto: " + tylerConcerto.getNumIngressos());
+        System.out.println("Quantidade de ingressos vendidos para Filme: " + filmeEvento.getNumIngressos());
+        System.out.println("Quantidade de ingressos vendidos para Teatro: " + teatroEvento.getNumIngressos());
     }
 }
