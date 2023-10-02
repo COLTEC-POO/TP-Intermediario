bf51c271f80d7e499b40950a3d04c183fad8b5ab
Davih Duque <davihduque8@gmail.com>
2023-10-02 19:55:32 -0300

    Testando Option Pane

--- src/Ingresso.java
+++ src/Ingresso.java
@@ -1,81 +1,74 @@
-import java.util.Date;
-
-public class Ingresso {
-
-    // Atributos base
-    protected Date data;
-    protected double preco = 30;
-    protected String tipo;
-
-    int VendaIngresso = 0;
-
-    // Construtor de Ingressos
-    public Ingresso(String tipo, double preco) {
-        this.data = new Date();
-        this.tipo = tipo;
-        this.preco = preco;
-    }
-
-    // Isso vai ser abstrato
-    // Funcao para pegar os detalhes para impressao como data, tipo, preco
-    public String toString() {
-        return this.data + " " + this.tipo + " " +  this.preco;
-    }
-
-    // Isso vai ser abstrato
-    public double getPreco() {
-        return preco;
-    }
-
-    // Isso vai ser abstrato
-    // Funcao para pegar o tipo de ingresso
-    public String getTipoIngresso() {
-        return tipo;
-    }
-
-    public static class ingressoPadrao extends Ingresso {
-
-        public ingressoPadrao(double preco) {
-            // Chama os dados da superclasse com o tipo e o preço ajustado para meia entrada
-            super("Ingresso Padrao", preco);
-            // Aumenta o numero de ingressos vendidos
-            VendaIngresso++;
-        }
-
-        @Override
-        public String toString() {
-            return this.data + "Ingresso Padrao" + this.preco;
-        }
-    }
-
-    // Subclasse meiaEntrada que representa um ingresso de meia entrada
-    public static class meiaEntrada extends Ingresso {
-
-        public meiaEntrada(double preco) {
-            // Chama os dados da superclasse com o tipo e o preço ajustado para meia entrada
-            super("Meia Entrada", preco * 0.5);
-            // Aumenta o numero de ingressos vendidos
-            VendaIngresso++;
-        }
-
-        @Override
-        public String toString() {
-            return this.data + "Meia Entrada" + this.preco;
-        }
-    }
-
-    public static class VIP extends Ingresso {
-
-        public VIP(double preco) {
-            // Chama os dados da superclasse com o tipo e o preço ajustado para meia entrada
-            super("VIP", preco * 2 );
-            // Aumenta o numero de ingressos vendidos
-            VendaIngresso++;
-        }
-
-        @Override
-        public String toString() {
-            return this.data + "VIP" + this.preco;
-        }
-    }
-}
+//import java.util.Date;
+//
+//public class Ingresso {
+//
+//    // Atributos base
+//    protected Date data;
+//    protected double preco = 30;
+//    protected String tipo;
+//
+//    // Construtor de Ingressos
+//    public Ingresso(String tipo, double preco) {
+//        this.data = new Date();
+//        this.tipo = tipo;
+//        this.preco = preco;
+//    }
+//
+//    // Isso vai ser abstrato
+//    // Funcao para pegar os detalhes para impressao como data, tipo, preco
+//    public String toString() {
+//        return this.data + " " + this.tipo + " " +  this.preco;
+//    }
+//
+//    // Isso vai ser abstrato
+//    public double getPreco() {
+//        return preco;
+//    }
+//
+//    // Isso vai ser abstrato
+//    // Funcao para pegar o tipo de ingresso
+//    public String getTipoIngresso() {
+//        return tipo;
+//    }
+//
+//    public static class ingressoPadrao extends Ingresso {
+//
+//        public ingressoPadrao(double preco) {
+//            // Chama os dados da superclasse com o tipo e o preço ajustado para meia entrada
+//            super("Ingresso Padrao", preco);
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return this.data + "Ingresso Padrao" + this.preco;
+//        }
+//    }
+//
+//    // Subclasse meiaEntrada que representa um ingresso de meia entrada
+//    public static class meiaEntrada extends Ingresso {
+//
+//        public meiaEntrada(double preco) {
+//            // Chama os dados da superclasse com o tipo e o preço ajustado para meia entrada
+//            super("Meia Entrada", preco * 0.5);
+//
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return this.data + "Meia Entrada" + this.preco;
+//        }
+//    }
+//
+//    public static class VIP extends Ingresso {
+//
+//        public VIP(double preco) {
+//            // Chama os dados da superclasse com o tipo e o preço ajustado para meia entrada
+//            super("VIP", preco * 2 );
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return this.data + "VIP" + this.preco;
+//        }
+//    }
+//}
--- src/mainTestes.java
+++ src/mainTestes.java
@@ -1,30 +1,35 @@
-// Classe principal para testar ingressos e eventos
-public class mainTestes {
-    public static void main(String[] args) {
-        double preco = 500;
-
-        Ingresso.ingressoPadrao padrao = new Ingresso.ingressoPadrao(preco);
-
-        Ingresso.meiaEntrada meia = new Ingresso.meiaEntrada(preco);
-
-        Ingresso.VIP  vip = new Ingresso.VIP(preco);
-
-        Eventos tylerConcerto = new Eventos.Concerto("Tyler", true);
-
-        // Limite de 3, então o último ingresso não é contabilizado
-        tylerConcerto.VenderIngressos(padrao);
-        tylerConcerto.VenderIngressos(meia);
-        tylerConcerto.VenderIngressos(vip);
-        tylerConcerto.VenderIngressos(padrao);
-
-        // Imprimindo a classe Evento
-        System.out.println(tylerConcerto.toString());
-
-        // Espaco em branco
-        System.out.println();
-
-        // Renda gerada pelo evento
-        tylerConcerto.imprimirExtrato();
-
-    }
-}
+//// Classe principal para testar ingressos e eventos
+//public class mainTestes {
+//    public static void main(String[] args) {
+//        double preco = 500;
+//
+//        Ingresso.ingressoPadrao padrao = new Ingresso.ingressoPadrao(preco);
+//
+//        Ingresso.meiaEntrada meia = new Ingresso.meiaEntrada(preco);
+//
+//        Ingresso.VIP  vip = new Ingresso.VIP(preco);
+//
+//        Eventos tylerConcerto = new Eventos.Concerto("Tyler", true);
+//        Eventos tylerFilme = new Eventos.Filme("TylerFilme", true);
+//
+//
+//        // Limite de 3, então o último ingresso não é contabilizado
+//        tylerConcerto.VenderIngressos(padrao);
+//        tylerConcerto.VenderIngressos(meia);
+//        tylerConcerto.VenderIngressos(vip);
+//        tylerConcerto.VenderIngressos(padrao);
+//
+//        // Imprimindo a classe Evento
+//        System.out.println(tylerConcerto.toString());
+//        System.out.println(tylerFilme.toString());
+//
+//        // Espaco em branco
+//        System.out.println();
+//
+//        // Renda gerada pelo evento
+//        tylerConcerto.imprimirExtrato();
+//
+//
+//
+//    }
+//}
