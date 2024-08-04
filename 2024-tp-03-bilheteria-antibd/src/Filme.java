c8a8086b61c04ac89dcb168961b6646586342af4
Davi Afonso Dias da Silva <143016968+githubersvaldo@users.noreply.github.com>
2024-08-04 13:37:33 -0300

    Add files via upload
    o que eu fiz até agora

---
+++ 2024-tp-03-bilheteria-antibd/src/Filme.java
@@ -0,0 +1,34 @@
+public class Filme extends Evento{
+    public Filme() {
+        //define a quantidade de ingrssos disponiveis para venda
+        QuanIn = 200;
+    }
+    private Ingresso[] IngressoFilm = new Ingresso[QuanIn];
+
+    @Override
+    public boolean VerificacaoDosIngressos() {
+        for(Ingresso atual : this.IngressoFilm)
+            if(atual != null)
+                //retorna false caso haja ingressos Vip
+                if(atual instanceof InGreVip )
+                    return false;
+        return true;
+    }
+
+    @Override
+    public double ReceiTot() {
+        double total = 0;
+        for(Ingresso atual : this.IngressoFilm){
+            if(atual!= null)
+                total += atual.getPreco();
+        }
+        return total;
+    }
+    @Override
+    public void extrato(){
+        for(Ingresso atual : this.IngressoFilm){
+            if(atual!= null)
+                atual.toString();
+        }
+    }
+}
